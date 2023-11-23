588aee870e4066716f52ce7b632663853bd9c608
Jônatas <jonatas.07022003@gmail.com>
2023-11-22 23:52:10 -0300

    Add files via upload

---
+++ desafio-tecnico-1.js
@@ -0,0 +1,12 @@
+function somatorio(num) {
+    let soma = 0;
+    for (let i = 1; i < num; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            soma += i;
+        }
+    }
+
+    return soma;
+}
+
+console.log(somatorio(10))
